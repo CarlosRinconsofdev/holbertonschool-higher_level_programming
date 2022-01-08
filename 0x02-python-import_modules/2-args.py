2409e2e2bfa0eb674157b008cb0fcf383ab47a95
CarlosRinconsofdev <3901@holbertonschool.com>
2022-01-07 19:46:38 -0500

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+num = len(argv)
+if num == 1:
+    print("0 arguments.")
+elif num == 2:
+    print("1 argument:")
+else:
+    print("{} arguments". format(num -1))
+for i in range (1, num):
+    print("{}: {}".format(i, argv[i]))
