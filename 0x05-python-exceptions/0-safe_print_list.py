8a97ce89d7b00c7992a850cc5f9c2a35fa8f7858
CarlosRinconsofdev <3901@holbertonschool.com>
2022-01-17 16:49:27 -0500

    fix the code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from multiprocessing.sharedctypes import Value
 
 
 def safe_print_list(my_list=[], x=0):
@@ -8,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError):
-            continue
+        except IndexError:
+            pass
     print()
     return count
