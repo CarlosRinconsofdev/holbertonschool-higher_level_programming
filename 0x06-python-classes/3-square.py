049ae2dda274dfb2a15bc9572ba97780f929792c
CarlosRinconsofdev <3901@holbertonschool.com>
2022-01-18 23:13:57 -0500

    task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" class that defines a square"""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """ Atributes: size (int): the size of the square """
+    def __init__(self, size=0):
+        """Initializes method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area of Square"""
+        return self.__size*self.__size
