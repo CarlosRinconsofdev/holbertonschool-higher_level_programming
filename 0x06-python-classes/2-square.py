ae036de247e6e74cdde1c7a511461839c72e6530
CarlosRinconsofdev <3901@holbertonschool.com>
2022-01-18 22:52:13 -0500

    task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class that defines a square"""
+
+
+from multiprocessing.sharedctypes import Value
+
+
+class Square:
+    """ Atributes: size (int): the size of the square """
+    def __init__(self, size=0):
+        """Initializes method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = size
