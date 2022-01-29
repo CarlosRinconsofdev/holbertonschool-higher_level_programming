8e83ef63cbc756db2d30d3ca1c1c505779eea706
CarlosRinconsofdev <3901@holbertonschool.com>
2022-01-28 20:30:17 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,19 +3,13 @@
 add_integer:
 write a function that adds 2 integers (a, b)
 and return the sum.
+
 """
 
 
 def add_integer(a, b=98):
     """
     Adds two integers
-
-    Args:
-        a (int): integer to add
-        b (int): integer to add
-
-    Raise:
-        TypeError: exception with the message.
     """
 
     if type(a) is not int:
@@ -28,4 +22,4 @@ def add_integer(a, b=98):
             b = int(b)
         else:
             raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
