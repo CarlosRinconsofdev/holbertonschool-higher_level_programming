13893638efaed96d0044c9da4c6366d2948ee5ed
CarlosRinconsofdev <3901@holbertonschool.com>
2022-02-08 23:12:57 -0500

    t2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,59 +11,59 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self._width = width
+        self._height = height
+        self._x = x
+        self._y = y
 
     def get_width(self):
         """
         getter for width value
         Reurn: Private width value
         """
-        return self.width
+        return self._width
 
     def set_width(self, width):
         """
         setter forr width value
         """
-        self.width = width
+        self._width = width
 
     def get_height(self):
         """
         getter for height value
         Return: Private height value
         """
-        return self.height
+        return self._height
 
     def set_height(self, height):
         """
         setter forr width value
         """
-        self.height = height
+        self._height = height
 
     def get_x(self):
         """
         Getter for x value
         Return: Private x value
         """
-        return self.x
+        return self._x
 
     def set_x(self, x):
         """
         setter for x value
         """
-        self.x = x
+        self._x = x
 
     def get_y(self):
         """
         Getter for y value
         Return: Private y value
         """
-        return self.y
+        return self._y
 
     def set_y(self, y):
         """
         setter for y value
         """
-        self.y = y
+        self._y = y
