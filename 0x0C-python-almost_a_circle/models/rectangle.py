fa1d0cb1e97732a2cc4c8697bb3296b6d3ee2a12
CarlosRinconsofdev <3901@holbertonschool.com>
2022-02-08 23:14:55 -0500

    t2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,49 +21,49 @@ class Rectangle(Base):
         getter for width value
         Reurn: Private width value
         """
-        return self._width
+        return self.__width
 
     def set_width(self, width):
         """
         setter forr width value
         """
-        self._width = width
+        self.__width = width
 
     def get_height(self):
         """
         getter for height value
         Return: Private height value
         """
-        return self._height
+        return self.__height
 
     def set_height(self, height):
         """
         setter forr width value
         """
-        self._height = height
+        self.__height = height
 
     def get_x(self):
         """
         Getter for x value
         Return: Private x value
         """
-        return self._x
+        return self.__x
 
     def set_x(self, x):
         """
         setter for x value
         """
-        self._x = x
+        self.__x = x
 
     def get_y(self):
         """
         Getter for y value
         Return: Private y value
         """
-        return self._y
+        return self.__y
 
     def set_y(self, y):
         """
         setter for y value
         """
-        self._y = y
+        self.__y = y
