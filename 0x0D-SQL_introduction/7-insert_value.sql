78eafbc8c3f940c024b7442525f11bb5512cbfba
CarlosRinconsofdev <3901@holbertonschool.com>
2022-03-16 00:01:48 -0500

    t7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- This script show how inster a row in a table in a DataBase in MySQL server
-INSERT INTO first_table (id, name) VALUES (89,'Best_school');
+INSERT INTO first_table (id, name) VALUES (89,'Best school');
