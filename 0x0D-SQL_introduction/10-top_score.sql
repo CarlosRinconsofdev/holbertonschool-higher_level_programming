fbf7350d26e9f6d5843b5c7f19e71f4e7a303e12
CarlosRinconsofdev <3901@holbertonschool.com>
2022-03-17 17:11:01 -0500

    t10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- This script show how to list all records in a second table 
 -- In a  DataBase in MySQL server
-SELECT name, score FROM second_table
-ORDER BY score DESC
+SELECT score, name FROM second_table
+ORDER BY score DESC;
