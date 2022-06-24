afeb43d89bdcd8cc59b7fd35005cdbd91178321b
CarlosRinconsofdev <3901@holbertonschool.com>
2022-06-24 12:13:27 -0500

    t0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Write a script that lists all
+states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db=MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+        rows = cursor.fetchall()
+
+    except MySQLdb.Error as e:
+        print("MYSQL Error: %s", str(e))
+
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+# Hello world - This is a new project. 0x0F. Python - Object-relational mapping
